a9433a199da4c1442cd7d2b2a43c31c1209c0039
Kalyan Manna <kalyanmanna439@gmail.com>
2026-02-18 15:27:14 +0530

    fixed

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,25 @@
-import NotFound from './NotFound'
+import Accordion from './pages/Accordion'
+import ChooseSmarter from './pages/ChooseSmarter'
+import Feedback from './pages/Feedback'
+import Footer from './pages/Footer'
+import Gain from './pages/Gain'
+import Get from './pages/Get'
+import Hero from './pages/Hero'
+import Solution from './pages/Solution'
+
+
 
 const Home = () => {
   return (
     <>
-      <NotFound/>
+      <Hero />
+      <Solution />
+      <Gain />
+      <ChooseSmarter />
+      <Get />
+      <Feedback />
+      <Accordion />
+      <Footer />
     </>
   )
 }
